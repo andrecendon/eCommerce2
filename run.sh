3bee325c52e44076649fe99d798b4315fa9c7a02
andrecendon <cendonandre@gmail.com>
2024-06-07 12:46:17 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Instala las dependencias
-
+python3 
 pip install Flask
 pip install flask
+
 pip install --upgrade pip setuptools
 pip install greenlet
 pip install -r requirements.txt
