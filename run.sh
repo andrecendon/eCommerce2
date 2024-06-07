9434a65f138ac0ce1a09cb0af1f2c3cf5db91518
andrecendon <cendonandre@gmail.com>
2024-06-07 11:58:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # Instala las dependencias
 pip install -r requirements.txt
 pip install greenlet  # Asegúrate de que greenlet esté instalado
-
+pip install flask
 # Ejecuta la aplicación Flask
 flask run
