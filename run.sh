f50e02e22cbab26bd6f17eea8ba7cadb8b3a326d
andrecendon <cendonandre@gmail.com>
2024-06-07 12:24:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ python3 -m venv env
 source env/bin/activate
 pip install Flask
 pip install flask
+gunicorn -w 4 -b 0.0.0.0:$PORT models.app:app
 pip install --upgrade pip setuptools
 pip install greenlet
 pip install -r requirements.txt
